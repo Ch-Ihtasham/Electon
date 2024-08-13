349a4945376411003dcb1819560157d1f2276f7d
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-08-13 16:42:35 +0500

    commit one

--- cart.js
+++ cart.js
@@ -1,67 +1,69 @@
-function add() {
-    var cartItemContainer = document.getElementById('cartItem');
-    var newItem = document.createElement('div');
-    newItem.innerHTML = `
-    <div class="items" >
-    <div class="product">
-        <div><img src="./Assets/speaker1.png" alt=""></div>
-        <div>
-            <div class="name">Speaker</div>
-            <div class="color">
-                <span>Color:</span>
-                <span>Black</span>
-            </div>
-        </div>
-    </div>
-    <div class="price">$1100</div>
-    <div class="inc">
-        <span>-</span>
-        <span>1</span>
-        <span>+</span>
-    </div>
-    <div class="price">
-        $1100
-    </div>
-    <div>
-        <img src="./Assets/close-circle.png" alt="" srcset="" class="cross">
-    </div>
+// function add() {
+//     var cartItemContainer = document.getElementById('cartItem');
+//     var newItem = document.createElement('div');
+//     newItem.innerHTML = `
+//     <div class="items" >
+//     <div class="product">
+//         <div><img src="./Assets/speaker1.png" alt=""></div>
+//         <div>
+//             <div class="name">Speaker</div>
+//             <div class="color">
+//                 <span>Color:</span>
+//                 <span>Black</span>
+//             </div>
+//         </div>
+//     </div>
+//     <div class="price">$1100</div>
+//     <div class="inc">
+//         <span>-</span>
+//         <span>1</span>
+//         <span>+</span>
+//     </div>
+//     <div class="price">
+//         $1100
+//     </div>
+//     <div>
+//         <img src="./Assets/close-circle.png" alt="" srcset="" class="cross">
+//     </div>
 
-</div>
-    `;
-    cartItemContainer.appendChild(newItem);
+// </div>
+//     `;
+//     cartItemContainer.appendChild(newItem);
 
-}
-function increment1() {
+// }
 
-    let num1 = document.getElementById("one");
-    let currentNum = parseInt(num1.innerText);
-    let result = currentNum + 1;
-    num1.innerText = result;
-
-    let num2 = document.getElementById("pkr");
-    let currentNum2 = parseInt(num2.innerText);
-    let product = currentNum2 * result;
-
-    let num3 = document.getElementById("bill");
-    num3.innerText = String(product);
-
-}
-function decrement1() {
-    let num1 = document.getElementById("one")
-    let currentNum = parseInt(num1.innerText);
-    if (currentNum > 0) {
-        var result = currentNum - 1;
-        num1.innerText = result;
-    }
-    let num2 = document.getElementById("pkr");
-    let currentNum2 = parseInt(num2.innerText);
-    let product = currentNum2 * result;
-
-    let num3 = document.getElementById("bill");
-    num3.innerText = String(product);
-
-}   
-let check=document.getElementById("btn")
-check.addEventListener('click',()=>{
+let check = document.getElementById("btn")
+check.addEventListener('click', () => {
     alert("Order Placed")
+})
+
+
+
+
+let decrement = document.querySelectorAll('.decrement')
+let increment = document.querySelectorAll('.increment')
+let count = document.querySelectorAll('.count')
+let price = document.querySelectorAll('.rs')
+let total = document.querySelectorAll('.total')
+increment.forEach((value, index) => {
+    value.addEventListener('click', () => {
+        let currentNumber = parseInt(count[index].innerHTML)
+        currentNumber++
+        count[index].innerHTML = currentNumber
+        total[index].innerHTML = currentNumber * parseInt(price[index].innerHTML)
+
+
+    })
+})
+decrement.forEach((value, index) => {
+    value.addEventListener('click', () => {
+        if (parseInt(count[index].innerHTML) > 0) {
+            let currentNumber = parseInt(count[index].innerHTML)
+            currentNumber--
+            count[index].innerHTML = currentNumber
+            total[index].innerHTML = currentNumber * parseInt(price[index].innerHTML)
+
+        }
+
+    })
 })
